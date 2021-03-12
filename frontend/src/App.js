a823f27750c796206332000de87fa9db941ebe99
rxngui <mateus2015men@outlook.com>
2021-03-12 20:01:49 -0300

    Teste Commit 2.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Teste de react app</h1>
+        <p>Teste commit Front end</p>
         <a
           className="App-link"
           href="https://reactjs.org"
