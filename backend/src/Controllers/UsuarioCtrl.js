f643f44cd437f70441b5136b4b2aa8f79c2c711e
Jose Paulo <jp_saturnino@hotmail.com>
2021-03-19 09:39:14 -0300

    alteracoes

--- backend/src/Controllers/UsuarioCtrl.js
+++ backend/src/Controllers/UsuarioCtrl.js
@@ -1,46 +1,59 @@
 const db = require('../models/Database');
 
 module.exports = {
-//cadastros básicos e login
+    //cadastros básicos e login
     async login(request, response) {
-        const {email, senha} = request.body;
-        const con = await db.conecta();
+        const { email, senha } = request.body;
+        await db.conecta();
         const sql = "SELECT * FROM Usuario WHERE usu_email = ? and usu_senha = ?";
         const valores = [email, senha];
         const result = await db.consulta(sql, valores);
         return response.json(result.data);
     },
+
     async listarUsuarios(request, response) {
-        const con = await db.conecta();
+        await db.conecta();
         const sql = "SELECT * FROM Usuario";
         const usuarios = await db.consulta(sql);
         return response.json(usuarios.data);
     },
-    async gravarUsuario(request, response){
-        const {usu_email, usu_senha, usu_nome, usu_dtNasc, usu_fone, usu_cpf, usu_sexo} = request.body;
-        const con = await db.conecta();
-        const sql = "INSERT INTO Usuario (usu_email, usu_senha, usu_nome, usu_dtNasc, usu_fone, usu_cpf, usu_sexo) "+
-        "VALUES (?, ?, ?, ?, ?, ?, ?)";
-        const valores = [usu_email, usu_senha, usu_nome, usu_dtNasc, usu_fone, usu_cpf, usu_sexo];
+
+    async gravarUsuario(request, response) {
+        const {
+            nome, sobrenome, email,
+            senha, cpf, endereco,
+            cidade, cep
+        } = request.body;
+        await db.conecta();
+        const sql = "INSERT INTO Usuario (usu_nome, usu_sobrenome," +
+            "usu_email, usu_senha, usu_cpf, " +
+            "usu_endereco, usu_cidade, usu_cep) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        const valores = [
+            nome, sobrenome, email,
+            senha, cpf, endereco, 
+            cidade, cep
+        ];
         const result = await db.manipula(sql, valores);
         return response.json(result);
     },
+
     async alterarUsuario(request, response) {
-        const {usu_id, usu_email, usu_senha, usu_nome, usu_dtNasc, usu_fone, usu_cpf, usu_sexo} = request.body;
-        const con = await db.conecta();
+        const { usu_id, usu_email, usu_senha, usu_nome, usu_dtNasc, usu_fone, usu_cpf, usu_sexo } = request.body;
+        await db.conecta();
         const sql = "UPDATE Usuario SET usu_email = ?, usu_senha = ?, usu_nome = ?, usu_dtNasc = ?, usu_fone = ?, usu_cpf = ?, usu_sexo = ? WHERE usu_id = ?";
         const valores = [usu_email, usu_senha, usu_nome, usu_dtNasc, usu_fone, usu_cpf, usu_sexo, usu_id];
         const result = await db.manipula(sql, valores);
         return response.json(result);
     },
+
     async excluirUsuario(request, response) {
-        const {usu_id} = request.params;
-        const con = await db.conecta();
+        const { usu_id } = request.params;
+        await db.conecta();
         const sql = "DELETE FROM Usuario WHERE usu_id = ?";
         const valor = [usu_id];
         const result = await db.manipula(sql, valor);
         console.log(result);
         return response.json(result);
-    },
-
+    }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,24 +3,24 @@ const routes = Router();
 
 const UsuCtrl = require('./Controllers/UsuarioCtrl');
 
-routes.post('/usuarios',UsuCtrl.gravarUsuario);
-routes.get('/usuarios',UsuCtrl.listarUsuarios);
-routes.put('/usuarios',UsuCtrl.alterarUsuario);
+routes.post('/usuarios/cadastro', UsuCtrl.gravarUsuario);
+routes.get('/usuarios', UsuCtrl.listarUsuarios);
+routes.put('/usuarios', UsuCtrl.alterarUsuario);
 routes.delete('/usuarios/:usu_id', UsuCtrl.excluirUsuario);
 routes.post('/usuarios/login', UsuCtrl.login);
 
 const AssCtrl = require('./Controllers/AssentoCtrl');
 
-routes.post('/assentos',AssCtrl.gravarAssento);
-routes.get('/assentos',AssCtrl.listarAssentos);
-routes.put('/assentos',AssCtrl.alterarAssento);
+routes.post('/assentos', AssCtrl.gravarAssento);
+routes.get('/assentos', AssCtrl.listarAssentos);
+routes.put('/assentos', AssCtrl.alterarAssento);
 routes.delete('/assentos/:ass_id', AssCtrl.excluirAssento);
 
 const CatCtrl = require('./Controllers/CategoriaCtrl');
 
-routes.post('/categorias',CatCtrl.gravarCategoria);
-routes.get('/categorias',CatCtrl.listarCategorias);
-routes.put('/categorias',CatCtrl.alterarCategoria);
+routes.post('/categorias', CatCtrl.gravarCategoria);
+routes.get('/categorias', CatCtrl.listarCategorias);
+routes.put('/categorias', CatCtrl.alterarCategoria);
 routes.delete('/categorias/:cat_id', CatCtrl.excluirCategoria);
 
 
