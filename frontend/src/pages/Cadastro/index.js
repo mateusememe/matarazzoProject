f2eb2e41c220f81136c8aee86dd88edfa5eab9f9
rxngui <mateus2015men@outlook.com>
2021-03-18 21:54:37 -0300

    Implementado o style do Cadastro

--- frontend/src/pages/Cadastro/index.js
+++ frontend/src/pages/Cadastro/index.js
@@ -7,10 +7,21 @@ export default function Cadastro() {
             <div className="form">
                 <h1 className="title">CADASTRO</h1>
                 <form>
-                    <input type="text" placeholder="Digite seu primeiro nome..."></input>
-                    <input type="text" placeholder="Digite seu sobrenome..."></input>
+                    <div className="flexName">
+                        <input style={{width: "45%"}}type="text" placeholder="Digite seu primeiro nome..."></input>
+                        <input style={{width: "45%"}} type="text" placeholder="Digite seu sobrenome..."></input>
+                    </div>
                     <input type="email" placeholder="Digite seu email..."></input>
-                    <input type="text" placeholder="Digite seu cpf..."></input>
+                    <input type="password" placeholder="Digite sua senha..."></input>
+                    <div className="flexName">
+                        <input style={{width: "55%"}} type="text" placeholder="Digite seu cpf..."></input>
+                        <input style={{width: "35%"}} type="date" ></input>
+                    </div>
+                    <input type="text" placeholder="Digite seu endereço (com o nº)..."></input>
+                    <div className="flexName">
+                        <input style={{width: "55%"}} type="text" placeholder="Digite o nome da sua Cidade..."></input>   
+                        <input style={{width: "35%"}} type="text" placeholder="Digite seu CEP..."></input>
+                    </div>
 
                 </form>
                 <a href="/login"><span>Ja possuo uma conta</span></a>
